13a013e8e65daf1009abb29c3c13d914de3b64d7
Fernando <fer3443@gmail.com>
2024-03-15 18:11:34 -0300

    nav-menu creado con funcion de mostrar y ocultar menu

--- src/script/index.js
+++ src/script/index.js
@@ -1,3 +1,13 @@
 "use strict";
 (() => {
+    const buttonClose = document.querySelector("#nav-close");
+    const buttonShow = document.querySelector("#nav-toggle");
+    const navMenu = document.querySelector("#nav-menu");
+    const handleMenu = () => {
+        if (navMenu) {
+            navMenu.classList.toggle("show-menu");
+        }
+    };
+    buttonClose.addEventListener("click", handleMenu);
+    buttonShow.addEventListener("click", handleMenu);
 })();
--- src/script/index.ts
+++ src/script/index.ts
@@ -1,3 +1,13 @@
 (() => {
-    
+  const buttonClose = document.querySelector("#nav-close") as HTMLDivElement;
+  const buttonShow = document.querySelector("#nav-toggle") as HTMLDivElement;
+  const navMenu = document.querySelector("#nav-menu") as HTMLDivElement;
+  const handleMenu = (): void => {
+    if (navMenu) {
+      navMenu.classList.toggle("show-menu");
+    }
+  };
+
+	buttonClose.addEventListener("click", handleMenu)
+	buttonShow.addEventListener("click", handleMenu)
 })();
